3ef135abffdbe9aca061254a5ab026722dd2786f
karen okasaki <oks.karen@gmail.com>
2023-02-06 14:25:52 -0300

    resolvido com o arquivo json

--- src/App.js
+++ src/App.js
@@ -8,54 +8,36 @@ import { Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 
 function App() {
-  const [allCountries, setAllCountries] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setIsLoading(true);
-    async function fetchCountries() {
-      const response = await axios.get(
-        `https://ih-countries-api.herokuapp.com/countries`
-      );
-      setAllCountries(response.data);
-      setIsLoading(false);
-    }
-
-    fetchCountries();
-  }, []);
+  const [allCountries, setAllCountries] = useState(countries);
 
   return (
     <div className="App">
       <Navbar />
-      {!isLoading && (
-        <div className="container">
-          <div className="row">
-            <div
-              className="col-5"
-              style={{ maxHeight: '90vh', overflow: 'scroll' }}
-            >
-              <div className="list-group">b
-                {allCountries
-                  .map((country) => {
-                    return (
-                      <div key={country.name.official}>
-                        <CountriesList country={country} />
-                      </div>
-                    );
-                  })
-                  .reverse()}
-              </div>
+      <div className="container">
+        <div className="row">
+          <div
+            className="col-5"
+            style={{ maxHeight: '90vh', overflow: 'scroll' }}
+          >
+            <div className="list-group">
+              {allCountries.map((country) => {
+                return (
+                  <div key={country.name.official}>
+                    <CountriesList country={country} />
+                  </div>
+                );
+              })}
             </div>
-
-            <Routes>
-              <Route
-                path="/:alpha3"
-                element={<CountryDetails allCountries={allCountries} />}
-              />
-            </Routes>
           </div>
+
+          <Routes>
+            <Route
+              path="/:alpha3"
+              element={<CountryDetails allCountries={allCountries} />}
+            />
+          </Routes>
         </div>
-      )}
+      </div>
     </div>
   );
 }
